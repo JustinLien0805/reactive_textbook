6f13f56d7c57eab4ae60014607082e38ba7ebc14
JustinLien0805 <6998dragon@gmail.com>
2023-03-07 18:07:48 +0800

    fixed form error

--- src/components/SignInForm.tsx
+++ src/components/SignInForm.tsx
@@ -10,12 +10,12 @@ type FormData = {
 const SignInForm = () => {
   const { register, handleSubmit } = useForm<FormData>();
   const onSubmit = (data: FormData) => {
-    navigate("/home")
+    navigate("/home");
   };
   const navigate = useNavigate();
   return (
     <div className="flex w-full max-w-lg flex-col items-center justify-center space-y-4 sm:w-3/4">
-      <h1 className="text-5xl font-bold">登入</h1>
+      <h1 className="text-5xl font-bold">See&Read</h1>
       <form
         onSubmit={handleSubmit(onSubmit)}
         className="flex w-full flex-col items-center justify-center space-y-4"
@@ -32,15 +32,23 @@ const SignInForm = () => {
           {...register("password")}
           className="input-bordered input w-full "
         />
-        <input type="submit" className="btn mx-4 w-full cursor-pointer" />
+        <button className="w-full text-right text-sm underline underline-offset-2">
+          忘記密碼？
+        </button>
+        <button type="submit" className="btn mx-4 w-full cursor-pointer">
+          登入
+        </button>
       </form>
       <div className="divider text-neutral">OR</div>
       <h2 className="text-sm text-neutral">
-        已經有帳號？{" "}
-        <button className="text-primary underline underline-offset-2" onClick={()=>{
-          navigate("/signup")
-        }}>
-          登入
+        沒有帳號嗎？{" "}
+        <button
+          className="text-primary underline underline-offset-2"
+          onClick={() => {
+            navigate("/signup");
+          }}
+        >
+          註冊
         </button>
       </h2>
     </div>
--- src/components/SignUpForm.tsx
+++ src/components/SignUpForm.tsx
@@ -14,7 +14,7 @@ const SignUpForm = () => {
 
   return (
     <div className="flex w-full max-w-lg flex-col items-center justify-center space-y-4 sm:w-3/4">
-      <h1 className="text-5xl font-bold">註冊</h1>
+      <h1 className="text-5xl font-bold">See&Read</h1>
       <form
         onSubmit={handleSubmit(onSubmit)}
         className="flex w-full flex-col items-center justify-center space-y-4"
@@ -31,19 +31,18 @@ const SignUpForm = () => {
           {...register("password")}
           className="input-bordered input w-full "
         />
-        <button className="w-full text-right text-sm underline underline-offset-2">
-          忘記密碼？
+        <button type="submit" className="btn mx-4 w-full cursor-pointer">
+          註冊
         </button>
-        <input type="submit" className="btn mx-4 w-full cursor-pointer" />
       </form>
       <div className="divider text-neutral">OR</div>
       <h2 className="text-sm text-neutral">
-        沒有帳號嗎？{" "}
+        已經有帳號了嗎？{" "}
         <button
           className="text-primary underline underline-offset-2"
           onClick={() => navigate("/signin")}
         >
-          註冊
+          登入
         </button>
       </h2>
     </div>
